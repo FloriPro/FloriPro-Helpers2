bb097c04d792c61ede05521c32cc902f1917b982
FloriPro <floriprolohner@gmail.com>
2022-09-20 18:49:30 +0200

    improvements

--- c/programs/testScript/main.js
+++ c/programs/testScript/main.js
@@ -7,8 +7,6 @@ class program extends System.program.default {
         console.log("started as id " + this.id);
         this.window = await SystemHtml.WindowHandler.createWindow("test",
             async () => {
-                console.log("ready")
-                console.log(this);
                 await this.window.setContent(`<p>hi</p><script>document.addEventListener("click",()=>{console.log("click")})</script>`);
             });
         this.window.close = () => {
--- c/programs/testScript/run.js
+++ c/programs/testScript/run.js
@@ -1,2 +1,1 @@
-console.log(PATH.folder());
 System.program.runProgram(PATH.folder() + "/main.js")
