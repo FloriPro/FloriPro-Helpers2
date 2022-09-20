095d8cca7544b53e73cc90c46445de5b2414b7c2
FloriPro <floriprolohner@gmail.com>
2022-09-20 18:34:51 +0200

    better testing

--- c/programs/testScript/main.js
+++ c/programs/testScript/main.js
@@ -5,8 +5,13 @@ class program extends System.program.default {
     }
     async init() {
         console.log("started as id " + this.id);
-        this.window = await SystemHtml.WindowHandler.createWindow("test");
-        this.window.close = () => { 
+        this.window = await SystemHtml.WindowHandler.createWindow("test",
+            async () => {
+                console.log("ready")
+                console.log(this);
+                await this.window.setContent(`<p>hi</p><script>document.addEventListener("click",()=>{console.log("click")})</script>`);
+            });
+        this.window.close = () => {
             this.stop()
             return true
         }
