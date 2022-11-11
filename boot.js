f60d35ed864bf33c991119b143b2c9139806abc9
FloriPro <floriprolohner@gmail.com>
2022-11-11 17:15:33 +0100

    moved bootjs to index.html

--- boot.js
+++
@@ -1,7 +0,0 @@
-async function load() {
-    if (localStorage["boot"] == undefined) {
-        localStorage["boot"] = await (await fetch("fileSysDownloaderBig.js")).text();
-    }
-    eval(localStorage["boot"])
-}
-load();
