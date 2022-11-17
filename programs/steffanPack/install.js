bef81e4a2f277bf05b007b3a933c662d8c5e0f9c
FloriPro <floriprolohner@gmail.com>
2022-11-17 16:33:15 +0100

    steffanPack

---
+++ programs/steffanPack/install.js
@@ -0,0 +1,7 @@
+async function run() {
+    await System.options.addValue("programs", "sfxSteffan", { "path": "c/programs/steffanPack/sfxmain.js", "name": "SFX-Steffan", "run": "c/programs/steffanPack/sfxrun.js" }, true);
+    await System.options.addValue("programs", "steffanFan", { "path": "c/programs/steffanPack/fanmain.js", "name": "Steffan-Fan", "run": "c/programs/steffanPack/fanrun.js" }, true);
+    SystemHtml.updateStartmenu()
+    return true;
+}
+run();
