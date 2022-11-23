5633b79ac92971fc0f4584233d5a2c22046218d0
FloriPro <floriprolohner@gmail.com>
2022-11-23 21:03:48 +0100

    gif is also an image

--- programs/imageViewer/install.js
+++ programs/imageViewer/install.js
@@ -1,6 +1,7 @@
 //file extensions
 System.options.addValue("fileExtensionOpener", "png", "c/programs/imageViewer/main.js", true);
 System.options.addValue("fileExtensionOpener", "jpg", "c/programs/imageViewer/main.js", true);
+System.options.addValue("fileExtensionOpener", "gif", "c/programs/imageViewer/main.js", true);
 
 System.options.addValue("programs", "imageViewer", { "path": "c/programs/Image Viewer/main.js", "name": "Image Viewer", "run": "c/programs/imageViewer/run.js" }, true);
 SystemHtml.updateStartmenu()
