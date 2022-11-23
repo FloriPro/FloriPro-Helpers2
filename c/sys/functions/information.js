893c0f748de4239329eb276c9a3501ab0a913f04
FloriPro <floriprolohner@gmail.com>
2022-11-23 21:03:35 +0100

    better informations

--- c/sys/functions/information.js
+++ c/sys/functions/information.js
@@ -11,15 +11,80 @@ class program extends System.program.default {
             //onready:
             async () => {
                 //set html
-                await this.window.setContent(await SystemFileSystem.getFileString(this.PATH.folder() + "/html.html"));
-                await this.window.size.setSize(200, 300)
-                await this.window.size.userCanResize(false)
+                await this.window.setContent(await SystemFileSystem.getFileString(this.PATH.folder() + "/information.html"));
+                await this.window.size.setSize(200, 300);
+                this.window.size.userCanResize(true);
 
+                this.howToUseList = await this.window.getHtmlElement("howToUseList");
+                this.howToUse = await SystemFileSystem.getFileJson(this.PATH.folder() + "/howToUse.json");
+                for (var x of Object.keys(this.howToUse)) {
+                    var b = document.createElement("button");
+                    b.innerText = x;
+                    b.setAttribute("element", x);
+                    this.howToUseList.append(b)
+                }
+                await this.window.parseNewHtml();
+                for (var x of Object.keys(this.howToUse)) {
+                    await this.window.addHtmlEventListener("onclick", x, (e) => {
+                        this.specificInfo(this.howToUse[e]);
+                    }, this, x)
+                }
             });
         this.window.close = () => {
             this.stop()
             return true
         }
+    }
+
+    async specificInfo(data) {
+        new class {
+            /**
+             * 
+             * @param {{[a:any]:string}} data 
+             */
+            constructor(data) {
+                this.data = data;
+                this.init();
+            }
+            async init() {
+                /**
+                 * @type {HtmlWindow}
+                 */
+                this.window = await SystemHtml.WindowHandler.createWindow("Information",
+                    //onready:
+                    async () => {
+                        //set html
+                        await this.window.setContent(`<div element="data"></div>`);
+                        await this.window.size.setSize(400, 300)
+                        this.window.size.userCanResize(true)
+
+                        var e = await this.window.getHtmlElement("data");
+                        for (var x of this.data) {
+                            if (x.type == "header") {
+                                var el = document.createElement("p");
+                                el.innerText = x.text;
+                                el.style.fontWeight = "bold";
+                                e.append(el);
+                            }
+                            else if (x.type == "text") {
+                                var el = document.createElement("p");
+                                el.innerText = x.text;
+                                e.append(el);
+                            }
+                            else if (x.type == "image") {
+                                var el = document.createElement("img");
+                                el.style.maxWidth = "100%";
+                                el.style.width = "300px";
+                                el.src = SystemFileSystem.toImg(await SystemFileSystem.getFileString(x.path));
+                                e.append(el);
+                            }
+                        }
+                    });
+                this.window.close = () => {
+                    return true;
+                }
+            }
+        }(data)
     }
 }
 new program();
--- dist/sys/functions/information.d.ts
+++ dist/sys/functions/information.d.ts
@@ -4,5 +4,8 @@ declare class program extends standardProg {
      * @type {HtmlWindow}
      */
     window: HtmlWindow;
+    howToUseList: HTMLElement;
+    howToUse: any;
+    specificInfo(data: any): Promise<void>;
 }
 //# sourceMappingURL=information.d.ts.map
