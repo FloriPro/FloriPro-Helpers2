cdcd9f4fecc20da70046a891268dbdafc38f7296
Florian Lohner <floriprolohner@gmail.com>
2023-01-10 15:42:44 +0100

    misnamed program

--- programs/coolFunctions/install.js
+++ programs/coolFunctions/install.js
@@ -1,5 +1,5 @@
 async function run() {
-    await System.options.addValue("programs", "coolFunctins", { "path": "c/programs/coolFunctions/main.js", "name": "Cool Functions", "run": "c/programs/coolFunctions/run.js" }, true);
+    await System.options.addValue("programs", "coolFunctions", { "path": "c/programs/coolFunctions/main.js", "name": "Cool Functions", "run": "c/programs/coolFunctions/run.js" }, true);
 
     await System.program.libInstall("chartjs")
 
