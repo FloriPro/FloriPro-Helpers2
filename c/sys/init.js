eace487080cca6821cf7f888144c275537739cae
FloriPro <floriprolohner@gmail.com>
2023-05-06 17:36:45 +0200

    new version

--- c/sys/init.js
+++ c/sys/init.js
@@ -25,7 +25,7 @@ window.onerror = function (error, url, line) {
     console.error(error);
 };
 
-VERSION = '1.1.5.5';
+VERSION = '1.1.5.6';
 SystemFileSystem = undefined;
 System = undefined
 SystemHtml = undefined;
