68cec0dce9a8edbe4c6ba74e9789908a8a592ea7
FloriPro <floriprolohner@gmail.com>
2023-05-16 21:21:59 +0200

    next version for settings

--- c/sys/init.js
+++ c/sys/init.js
@@ -25,7 +25,7 @@ window.onerror = function (error, url, line) {
     console.error(error);
 };
 
-VERSION = '1.1.5.7';
+VERSION = '1.1.5.8';
 SystemFileSystem = undefined;
 System = undefined
 SystemHtml = undefined;
